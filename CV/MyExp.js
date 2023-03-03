a856bb3492c5cf9249ccc0993048219a3c518038
BILTRESSE Sébastien <biltresse.sebastient@gmail.com>
2023-03-03 11:47:55 +0100

    SetInfo_01 : Personnal and Exp

---
+++ CV/MyExp.js
@@ -0,0 +1,8 @@
+let formations = [
+    {
+        "intitulé": "Développeur Web Apps"
+        , "Nom Entreprise": "TechnofuturTIC"
+        , "Année début": 2023
+        , "Année fin": 2023
+    }
+];
---
+++ CV/MyInfo.js
@@ -0,0 +1,6 @@
+let nom = "BILTRESSE"
+let prenom = "Sébastien"
+let ddn = "24 Mars 1984"
+let email = "biltresse.sebastien@gmail.com"
+let adress = ["Chaussée de Mons", 10, "F", 5530, "Spontin"]
+let tel = "+32 478 38 11 75"
